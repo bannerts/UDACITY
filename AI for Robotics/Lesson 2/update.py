846fc04d9e8bbff594dbf1f184ed0035eaecb514
Scott Bannert <bannerts@users.noreply.github.com>
2017-01-02 08:51:16 -0600

    Update update.py

--- AI for Robotics/Lesson 2/update.py
+++ AI for Robotics/Lesson 2/update.py
@@ -1,18 +1,35 @@
-# Write a program that will predict your new mean
-# and variance given the mean and variance of your 
-# prior belief and the mean and variance of your 
-# motion. 
+# Write a program that will iteratively update and
+# predict based on the location measurements 
+# and inferred motions shown below. 
 
-# PARAMETER UPDATE
 def update(mean1, var1, mean2, var2):
-    new_mean = (var2 * mean1 + var1 * mean2) / (var1 + var2)
-    new_var = 1/(1/var1 + 1/var2)
+    new_mean = float(var2 * mean1 + var1 * mean2) / (var1 + var2)
+    new_var = 1./(1./var1 + 1./var2)
     return [new_mean, new_var]
 
-# MOTION UPDATE
 def predict(mean1, var1, mean2, var2):
     new_mean = mean1 + mean2
     new_var = var1 + var2
     return [new_mean, new_var]
 
-print predict(10., 4., 12., 4.)
+measurements = [5., 6., 7., 9., 10.]
+motion = [1., 1., 2., 1., 1.]
+measurement_sig = 4.
+motion_sig = 2.
+mu = 0.
+sig = 10000.
+
+#Please print out ONLY the final values of the mean
+#and the variance in a list [mu, sig]. 
+
+mu0 = mu
+sig0 = sig
+for i in range(len(measurements)):
+    mm = measurements[i]
+    mot = motion[i]
+    [mu1, sig1] = update(mu0, sig0, mm, measurement_sig)
+    [mu0, sig0] = predict(mu1, sig1, mot, motion_sig)
+
+mu = mu0
+sig = sig0
+print [mu, sig]
