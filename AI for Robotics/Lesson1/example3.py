5f9fb61eb716d49ccae17e40ccd6f832f5f23998
Scott Bannert <bannerts@users.noreply.github.com>
2017-01-02 07:11:39 -0600

    Update example3.py

--- AI for Robotics/Lesson1/example3.py
+++ AI for Robotics/Lesson1/example3.py
@@ -1,1 +1,116 @@
+# The function localize takes the following arguments:
+#
+# colors:
+#        2D list, each entry either 'R' (for red cell) or 'G' (for green cell)
+#
+# measurements:
+#        list of measurements taken by the robot, each entry either 'R' or 'G'
+#
+# motions:
+#        list of actions taken by the robot, each entry of the form [dy,dx],
+#        where dx refers to the change in the x-direction (positive meaning
+#        movement to the right) and dy refers to the change in the y-direction
+#        (positive meaning movement downward)
+#        NOTE: the *first* coordinate is change in y; the *second* coordinate is
+#              change in x
+#
+# sensor_right:
+#        float between 0 and 1, giving the probability that any given
+#        measurement is correct; the probability that the measurement is
+#        incorrect is 1-sensor_right
+#
+# p_move:
+#        float between 0 and 1, giving the probability that any given movement
+#        command takes place; the probability that the movement command fails
+#        (and the robot remains still) is 1-p_move; the robot will NOT overshoot
+#        its destination in this exercise
+#
+# The function should RETURN (not just show or print) a 2D list (of the same
+# dimensions as colors) that gives the probabilities that the robot occupies
+# each cell in the world.
+#
+# Compute the probabilities by assuming the robot initially has a uniform
+# probability of being in any cell.
+#
+# Also assume that at each step, the robot:
+# 1) first makes a movement,
+# 2) then takes a measurement.
+#
+# Motion:
+#  [0,0] - stay
+#  [0,1] - right
+#  [0,-1] - left
+#  [1,0] - down
+#  [-1,0] - up
 
+def localize(colors,measurements,motions,sensor_right,p_move):
+    # initializes p to a uniform distribution over a grid of the same dimensions as colors
+    pinit = 1.0 / float(len(colors)) / float(len(colors[0]))
+    p = [[pinit for row in range(len(colors[0]))] for col in range(len(colors))]
+    
+    # >>> Insert your code here <<<
+    def movement(p_in, motion, p_move):
+        n_rows = len(p_in)
+        n_columns = len(p_in[0])
+        p_row = []
+        for i in range(n_rows):
+            p_column = []
+            for j in range(n_columns):
+                pmove = p_move * p_in[(i+motion[0])%n_rows][(j+motion[1])%n_columns]
+                pstay = (1-p_move) * p_in[i][j]
+                p_column.append(pmove+pstay)
+            p_row.append( p_column)
+        return p_row
+    
+    def normalize(p_in):
+        n_rows = len(p_in)
+        n_columns = len(p_in[0])
+        p_sum = 0
+        for i in range(n_rows):
+            for j in range(n_columns):
+                p_sum += p_in[i][j]
+        return [[p_in[i][j]/p_sum for j in range(n_columns)] for i in range(n_rows)]
+    
+    def measurement(colors, p_in, measurement, p_sensor):
+        n_rows = len(p_in)
+        n_columns = len(p_in[0])
+        p_row = []
+        for i in range(n_rows):
+            p_column = []
+            for j in range(n_columns):
+                if measurement == colors[i][j]:
+                    pmult = p_sensor
+                else:
+                    pmult = 1.0-p_sensor
+                pright = p_in[i][j] * p_sensor * measurement == colors
+                pright = (1-p_move) * p_in[i][j]
+                p_column.append(p_in[i][j] * pmult)
+            p_row.append(p_column)
+        return normalize(p_row)
+    
+    for n in range(len(motions)):
+        p = movement(p, motions[n], p_move)
+        p = measurement(colors, p, measurements[n], p_sensor)
+    
+    return p
+
+def show(p):
+    rows = ['[' + ','.join(map(lambda x: '{0:.5f}'.format(x),r)) + ']' for r in p]
+    print '[' + ',\n '.join(rows) + ']'
+    
+#############################################################
+# For the following test case, your output should be 
+# [[0.01105, 0.02464, 0.06799, 0.04472, 0.02465],
+#  [0.00715, 0.01017, 0.08696, 0.07988, 0.00935],
+#  [0.00739, 0.00894, 0.11272, 0.35350, 0.04065],
+#  [0.00910, 0.00715, 0.01434, 0.04313, 0.03642]]
+# (within a tolerance of +/- 0.001 for each entry)
+
+colors = [['R','G','G','R','R'],
+          ['R','R','G','R','R'],
+          ['R','R','G','G','R'],
+          ['R','R','R','R','R']]
+measurements = ['G','G','G','G','G']
+motions = [[0,0],[0,1],[1,0],[1,0],[0,1]]
+p = localize(colors,measurements,motions,sensor_right = 0.7, p_move = 0.8)
+show(p) # displays your answer
