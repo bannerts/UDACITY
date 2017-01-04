4a09370ce701cf0eb6e2899f1d482592f09cc54f
Scott Bannert <bannerts@users.noreply.github.com>
2017-01-04 17:55:07 -0600

    Create Search2.py

---
+++ AI for Robotics/Lesson4/Search2.py
@@ -0,0 +1,90 @@
+# -----------
+# User Instructions:
+#
+# Modify the the search function so that it returns
+# a shortest path as follows:
+# 
+# [['>', 'v', ' ', ' ', ' ', ' '],
+#  [' ', '>', '>', '>', '>', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', '*']]
+#
+# Where '>', '<', '^', and 'v' refer to right, left, 
+# up, and down motions. Note that the 'v' should be 
+# lowercase. '*' should mark the goal cell.
+#
+# You may assume that all test cases for this function
+# will have a path from init to goal.
+# ----------
+
+grid = [[0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0]]
+init = [0, 0]
+goal = [len(grid)-1, len(grid[0])-1]
+cost = 1
+
+delta = [[-1, 0 ], # go up
+         [ 0, -1], # go left
+         [ 1, 0 ], # go down
+         [ 0, 1 ]] # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+def search(grid,init,goal,cost):
+    # ----------------------------------------
+    # modify code below
+    # ----------------------------------------
+    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed[init[0]][init[1]] = 1
+    expand1 = [[[[0,0], ' '] for row in range(len(grid[0]))] for col in range(len(grid))]
+    expand = [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
+    x = init[0]
+    y = init[1]
+    g = 0
+
+    open = [[g, x, y]]
+
+    found = False  # flag that is set when search is complete
+    resign = False # flag set if we can't find expand
+
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
+            return 'fail'
+        else:
+            open.sort()
+            open.reverse()
+            next = open.pop()
+            x = next[1]
+            y = next[2]
+            g = next[0]
+            
+            if x == goal[0] and y == goal[1]:
+                found = True
+            else:
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost
+                            open.append([g2, x2, y2])
+                            closed[x2][y2] = 1
+                            expand1[x2][y2] = [[x, y], delta_name[i]]
+    if found:
+        last = goal
+        while (last != init):
+            xl = last[0]
+            yl = last[1]
+            p2, ch  = expand1[xl][yl]
+            expand[p2[0]][p2[1]] = ch
+            last = p2
+
+        
+    return expand # make sure you return the shortest path
+expand = search(grid,init,goal,cost)
+print expand
