b7955463c429498b8303c2581c2767bfe0e4f9b2
Scott Bannert <bannerts@users.noreply.github.com>
2017-01-05 00:43:59 -0600

    Update Quiz.py

--- AI for Robotics/Lesson4/Quiz.py
+++ AI for Robotics/Lesson4/Quiz.py
@@ -58,5 +58,77 @@ cost = [2, 1, 20] # cost has 3 values, corresponding to making
 # ----------------------------------------
 
 def optimum_policy2D(grid,init,goal,cost):
+    
+    value = [[ [999, 999, 999, 999] for row in range(len(grid[0]))] for col in range(len(grid))] 
+    pa = [[ [-1, -1, -1, -1] for row in range(len(grid[0]))] for col in range(len(grid))]
+    policy2D =  [[' ' for row in range(len(grid[0]))] for col in range(len(grid))] 
+    
+    def heuristic(x, y, d):
+        h = 0
+        isvalid = True
+        if not (d in range(len(forward))):
+            return h, False
+        elif not (x in range(len(grid))):
+            return h, False
+        elif not (y in range(len(grid[x]))):
+            return h, False
+        elif grid[x][y] == 1:
+            return h, False
+        return 0, True
+    
+    
+    x0 = init[0]
+    y0 = init[1]
+    d0 = init[2]
+    g0 = 0
+    h0, _ = heuristic(x0, y0, d0)
+    f0 = g0 + h0
+    open = [ [ (f0,g0,h0), x0, y0, d0 ] ]
+    value[x0][y0][d0] = g0
+    found = False
+    resign = False
+    count = 0
+    dirs = ['up', 'left', 'down', 'right']
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
+            return 'Fail'
+        open.sort(reverse=True)
+        next = open.pop()
+        [(fi, gi, hi), xi, yi, di] = next
+        if [xi, yi] == goal:
+            found = True
+        else:
+            for i in range(len(action)):
+                d = (di + action[i]) % len(forward);
+                x = xi + forward[d][0]
+                y = yi + forward[d][1]
+                g = gi + cost[i]
+                h, isvalid = heuristic(x, y, d)            
+                f = g + h
+                # print x, y, dirs[di], dirs[d], isvalid
+                if isvalid:
+                    if g <= value[x][y][d]:
+                        value[x][y][d] = g
+                        pa[x][y][d] = i
+                        # print x, y, dirs[d]
+                        open.append([(f, g, h), x, y, d])
+    
+    if found:
+        policy2D[xi][yi] = '*'
 
+        while [xi, yi, di] != init:
+            # print xi, yi, dirs[di]
+            i = pa[xi][yi][di]
+            a_name = action_name[i]
+            a = action[i]
+            [dx, dy] = forward[di]
+            xi = xi - dx
+            yi = yi - dy
+            di = (di - a) % (len(forward))
+            policy2D[xi][yi] = a_name
+    
     return policy2D
+
+out = optimum_policy2D(grid,init,goal,cost)
+print out
