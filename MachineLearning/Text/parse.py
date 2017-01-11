3f054f495569bc77181d910fa2a43fff3763d92c
Scott Bannert <bannerts@users.noreply.github.com>
2017-01-10 19:52:39 -0600

    Create parse.py

---
+++ MachineLearning/Text/parse.py
@@ -0,0 +1,28 @@
+from nltk.stem.snowball import SnowballStemmer
+import string
+
+def parseOutText(Data, FILE = False, STEMMER = True, LANGUAGE = 'english'):
+	""" Data (as text by default or file), parse out all text and use stemmer (nltk.stem.snowball.SnowballStemmer). 
+	"""
+	# Transform Data into content
+	if Not FILE:
+		## remove punctuation
+		text_string = Data.translate(string.maketrans("", ""), string.punctuation)
+	else:
+		## Data is a file
+    	Data.seek(0)  ### go back to beginning of file (annoying)
+    	all_text = Data.read()
+		### split off metadata
+		content = all_text.split("X-FileName:")
+		if len(content) > 1:
+       		### remove punctuation
+       		text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
+	words = ''
+	stemmer = SnowballStemmer(language)
+	for word in string.split(text_string):
+		if STEMMER:
+			stem_word = stemmer.stem(word)
+			words = words + stem_word + ' '
+		else:
+			words += word + ' '
+	return words
